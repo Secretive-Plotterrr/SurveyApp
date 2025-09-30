0c25832332da4258227f1b64271dc5573d23e137
Rico <ricovillamin2003@example.com>
2025-09-30 11:16:43 +0800

    Changes

--- app/src/Auth/SignUp.jsx
+++ app/src/Auth/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signUp } from './api'; // Adjust path based on your folder structure
+import { signUp } from '../utils/auth.js'; // Adjust path based on your folder structure
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
